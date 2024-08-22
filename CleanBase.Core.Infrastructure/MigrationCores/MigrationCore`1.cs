3416128acce110f642da140fcd0a9cd56cfeec2b
hoangvh238 <hoangvh238.dev@gmail.com>
2024-08-22 17:57:29 +0700

    add custom config migration

---
+++ CleanBase.Core.Infrastructure/MigrationCores/MigrationCore`1.cs
@@ -0,0 +1,28 @@
+﻿using CleanBase.Core.Settings;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanBase.Core.Infrastructure.MigrationCores
+{
+    public class MigrationCore<T> : Migration where T : AppSettings, new()
+    {
+        protected readonly T Settings;
+
+        public MigrationCore()
+        {
+            Settings = AppSettings<T>.Instance ?? new T { Schema = "CleanBase" };
+        }
+
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            if (!string.IsNullOrEmpty(Settings.Schema))
+            {
+                migrationBuilder.EnsureSchema(Settings.Schema);
+            }
+        }
+    }
+}
---
+++ CleanBase.Core.Infrastructure/MigrationCores/ModelSnapshotCore`1.cs
@@ -0,0 +1,25 @@
+﻿using CleanBase.Core.Settings;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanBase.Core.Infrastructure.MigrationCores
+{
+    public class ModelSnapshotCore<T> : ModelSnapshot where T : AppSettings, new()
+    {
+        protected T Settings { get; private set; }
+
+        public ModelSnapshotCore()
+        {
+            Settings = AppSettings<T>.Instance ?? new T { Schema = "CleanBase" };
+        }
+
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
