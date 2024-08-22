a58fcae6655bbb63d2037b07dd2f718a1bfc81f5
hoangvh238 <hoangvh238.dev@gmail.com>
2024-08-22 17:57:02 +0700

    add hosting manager job

---
+++ CleanBase.Core.Infrastructure/Jobs/Hosting/HostedService.cs
@@ -0,0 +1,35 @@
+﻿using CleanBase.Core.Services.Jobs;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanBase.Core.Infrastructure.Jobs.Hosting
+{
+	public class HostedService : IHostedService
+	{
+		IEnumerable<IProcessingJobConsumer> Jobs { get; set; }
+		public HostedService(IEnumerable<IProcessingJobConsumer> jobs)
+		{
+			Jobs = jobs;
+		}
+
+		public virtual async Task StartAsync(CancellationToken cancellationToken)
+		{
+			foreach (IProcessingJobConsumer consumer in Jobs)
+			{
+				await consumer.StartAsync(cancellationToken);
+			}
+		}
+
+		public virtual async Task StopAsync(CancellationToken cancellationToken)
+		{
+			foreach(IProcessingJobConsumer consumer in Jobs)
+			{
+				await consumer.StopAsync(cancellationToken);	
+			}
+		}
+	}
+}
---
+++ CleanBase.Core.Infrastructure/Jobs/JobBootStrapper.cs
@@ -0,0 +1,22 @@
+﻿using CleanBase.Core.Infrastructure.Jobs.Hangfire;
+using CleanBase.Core.Infrastructure.Jobs.Hosting;
+using CleanBase.Core.Services.Jobs;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanBase.Core.Infrastructure.Jobs
+{
+	public static class JobBootStrapper
+	{
+		public static IServiceCollection UseHangfireBackgroundJob(this IServiceCollection services)
+		{
+			services.AddHostedService<HostedService>();
+			services.AddSingleton<IBackgroundJob, HangfireBackgroundJob>();
+			return services;
+		}
+	}
+}
